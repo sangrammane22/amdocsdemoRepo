b6e1819a0e0bd5cafa416193e7ad843b13567df5
sangrammane22 <sangram.mane24@outlook.com>
2022-06-05 00:06:39 +0530

    Update HelloWorld.java

--- HelloWorld.java
+++ HelloWorld.java
@@ -2,6 +2,6 @@ public class TestGitDesktopFile {
 
     public static void main (String[] args) {
         System.out.println("Hello WORLD !!!");
-
+	System.out.println("I am in the Java Project. Have a Nice Day.");
     }
 }
